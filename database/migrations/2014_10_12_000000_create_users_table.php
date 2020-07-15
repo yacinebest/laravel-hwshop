557259cf6c558452d9301d72829f80125013c3ce
yacine.best <yacine.besta@gmail.com>
2020-07-15 09:22:40 +0100

    init project

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('username');
+            $table->enum('group',['user','admin'])->default('user');
+
+            $table->string('address')->nullable();
+
+
+            $table->date('birth_date')->nullable();
+            $table->enum('gender',['Male','Female'])->nullable();
+            $table->string('country')->nullable();
+            $table->string('phone_number')->nullable();
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
---
+++ database/migrations/2020_07_14_230305_create_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->uuid('user_id');
+            $table->date('order_date');
+            $table->enum('status',['processing','canceled','approved'])->default('processing');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
