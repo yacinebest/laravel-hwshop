f91f0c93770f14c52ad864438fb6b473ec619ecf
yacine.best <yacine.besta@gmail.com>
2020-07-16 22:37:40 +0100

    adding RoleFactory

--- tests/Unit/UserUnitTest.php
+++ tests/Unit/UserUnitTest.php
@@ -2,17 +2,33 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
 
 class UserUnitTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    use RefreshDatabase;
+    use DatabaseMigrations;
+
+    protected $user;
+
+    protected function setUp(): void
     {
-        $this->assertTrue(true);
+        parent::setUp();
+        factory(Role::class)->create(['type'=>'USER']);
+        factory(Role::class)->create(['type'=>'ADMIN']);
+        $this->user = factory(User::class)->create();
+    }
+
+     /**
+     * @test
+     * @return void
+    */
+    function can_create_user_with_factory()
+    {
+        $this->assertNotEmpty($this->user);
     }
 }
