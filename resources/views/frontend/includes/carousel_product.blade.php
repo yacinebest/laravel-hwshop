c706b68d7ad63dfb3951a2344640de05a83a793b
yacine.best <yacine.besta@gmail.com>
2020-09-10 20:30:40 +0100

    finish refactor product page

--- resources/views/frontend/includes/carousel_product.blade.php
+++ resources/views/frontend/includes/carousel_product.blade.php
@@ -7,33 +7,7 @@
             <section class="products clearfix text-center">
                 <div class="owl-carousel owl-theme align-content-around">
                     @foreach($products as $product)
-                        <div class="item p-2">
-                            <article class="product">
-                                <div class="container" style="height: 200px !important; width: 200px !important;">
-                                    <a href="{{ route('product',$product->id)  }}">
-                                        <img class="mw-100 mh-100" src="{{ $product->image ? $product->image->imagePath : 'https://picsum.photos/seed/picsum/380/225' }}">
-                                    </a>
-                                </div>
-                                <div class="container" style="height: 50px !important; width: 200px">
-                                    <h5 class="mw-100 mh-100 embed-responsive">
-                                        <a href="{{ route('product',$product->id)  }}">
-                                            {{ $product->name }}
-                                        </a>
-                                    </h5>
-                                </div>
-                                <div class="price-group">
-                                    <div class="price text-success">
-                                    <span class="currency">DZD</span>
-                                    <span>{{ $product->price }}</span>
-                                    </div>
-                                </div>
-                                <div class="btngroup">
-                                    <button type="button" class="btn btn-primary fa fa-shopping-cart ajouter-panier" title="Add To Cart">
-                                        Add To Cart
-                                    </button>
-                                </div>
-                            </article>
-                        </div>
+                        @include('frontend.products.card_product',['product'=>$product,'page'=>'carousel'])
                     @endforeach
                 </div>
             </section>
--- resources/views/frontend/products/card_product.blade.php
+++ resources/views/frontend/products/card_product.blade.php
@@ -1,24 +1,48 @@
-{{-- $page == home || category --}}
+{{-- $page == home || category || carousel--}}
 <div class="{{ $page=='home' || $page=='category' ? 'col-lg-4 col-md-6 ' : '' }} {{ $page=='carousel' ? 'item p-2' : '' }}">
-    <figure class="card card-product-grid {{ $page=='home' ? 'shadow' : '' }}">
-        <div class="img-wrap img-height {{ $page=='category' ? 'text-center' : '' }}">
-            <img class="img-height" src="{{ $product->image ? $product->image->imagePath : 'https://picsum.photos/seed/picsum/380/225' }}" style="{{ $page=='category' && $product->copy_number>0 ? '' : '-webkit-filter: grayscale(100%); filter: grayscale(100%);' }}">
+    <figure class=" {{ $page=='home' || $page=='category' ? 'card card-product-grid ' : ''}}
+                     {{ $page=='home' ? 'shadow ' : '' }}
+                     {{ $page=='carousel' ? 'product' : '' }}">
+
+        <div class=" {{ $page=='home' || $page=='category' ? 'img-wrap img-height ' : ''}}
+                     {{ $page=='category' ? 'text-center ' : '' }}
+                     {{ $page=='carousel' ? 'container ' : '' }}"  style="{{ $page=='carousel' ? 'height: 200px !important; width: 200px !important; ' : '' }}">
+
+                <img class="{{ $page=='home' || $page=='category' ? 'img-height ' : ''}}
+                            {{ $page=='carousel' ? 'mw-100 mh-100 ' : '' }}"
+                     src="{{ $product->image ? $product->image->imagePath : 'https://picsum.photos/seed/picsum/380/225' }}" style="{{ $page=='category' && $product->copy_number>0 ? '' : '-webkit-filter: grayscale(100%); filter: grayscale(100%);' }}">
                 <a class="btn-overlay" href="{{ route('product',$product->id) }}">
                     <i class="fa fa-search-plus"></i> See Product
                 </a>
         </div>
 
         <figcaption class="info-wrap">
-            <div class="text-center" style=" {{ $page=='category' ? 'min-height: 104px; ' : ' '  }} {{ $page=='home' ? 'height: 85px !important; ' : ' '  }} ">
-                <a href="{{ route('product',$product->id) }}" class="title h5 text-capitalize {{ $page=='home' ? 'mw-100 mh-100 embed-responsive ' : ' '  }} {{ $page=='category' && $product->copy_number==0 ? 'text-secondary' : ''}} "  >
+            <div class="{{ $page=='home' || $page=='category' ? 'text-center ' : ''}}
+                        {{ $page=='carousel' ? 'container ' : '' }} "
+                style=" {{ $page=='category' ? 'min-height: 104px; ' : ' '  }}
+                        {{ $page=='home' ? 'height: 85px !important; ' : ' '  }}
+                        {{ $page=='carousel' ? 'height: 50px !important; width: 200px ' : '' }} ">
+
+                <a href="{{ route('product',$product->id) }}" class="title h5 text-capitalize {{ $page=='home' || $page=='carousel' ? 'mw-100 mh-100 embed-responsive ' : ' '  }} {{ $page=='category' && $product->copy_number==0 ? 'text-secondary' : ''}} "  >
                     {{ $product->name }}
                 </a>
-                <div class="price-wrap mt-2 price-product">
-                    <span class="price">{{ $product->price }} DZD</span>
-                </div>
+                @if($page=='carousel')
+                    <div class="price-group">
+                        <div class="price text-success">
+                        <span class="currency">DZD</span>
+                        <span>{{ $product->price }}</span>
+                        </div>
+                    </div>
+                @else
+                    <div class="price-wrap mt-2 price-product">
+                        <span class="price">{{ $product->price }} DZD</span>
+                    </div>
+                @endif
             </div>
+
             @if($product->copy_number>0)
-                <a href="#" class="btn btn-block btn-primary mt-2 ajouter-panier ">
+                <a href="#" class="{{ $page=='home' || $page=='category' ? 'btn btn-block btn-primary mt-2 ajouter-panier ' : ''}}
+                                    {{ $page=='carousel' ? 'btn btn-primary fa fa-shopping-cart ajouter-panier ' : '' }}   ">
                     Add To Cart
                 </a>
             @else
@@ -26,37 +50,4 @@
             @endif
         </figcaption>
     </figure>
-</div>
-
-
-{{--  --}}
-{{--  --}}
-{{-- carousel  --}}
-
-{{-- <div class="item p-2"> --}}
-    <article class="product">
-        <div class="container" style="height: 200px !important; width: 200px !important;">
-            <a href="{{ route('product',$product->id)  }}">
-                <img class="mw-100 mh-100" src="{{ $product->image ? $product->image->imagePath : 'https://picsum.photos/seed/picsum/380/225' }}">
-            </a>
-        </div>
-        <div class="container" style="height: 50px !important; width: 200px">
-            <h5 class="mw-100 mh-100 embed-responsive">
-                <a href="{{ route('product',$product->id)  }}">
-                    {{ $product->name }}
-                </a>
-            </h5>
-        </div>
-        <div class="price-group">
-            <div class="price text-success">
-            <span class="currency">DZD</span>
-            <span>{{ $product->price }}</span>
-            </div>
-        </div>
-        <div class="btngroup">
-            <button type="button" class="btn btn-primary fa fa-shopping-cart ajouter-panier" title="Add To Cart">
-                Add To Cart
-            </button>
-        </div>
-    </article>
 </div>
